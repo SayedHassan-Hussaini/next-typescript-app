67b93f4ad72e575d36d17a027096b1ae4abe3409
Sayedhassan-Hussaini <sh.hussaini1378@gmail.com>
2023-01-03 09:36:10 +0430

    fix all in product list

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,30 @@ import Card from "../components/card/Card-component";
 import { shopifyClient, parseShopifyResponse } from "../lib/shopify";
 
 interface IIndex {
-  products: [];
+  products: [id: string, descriptionHtml: string, images: [], handle: string];
 }
+type productType = {
+  id: string;
+  images: [];
+  handle: string;
+  variants: [];
+};
 const Index = ({ products }: IIndex) => {
   console.log("products...........", products);
 
   return (
     <div className="md:w-[80%] mx-auto mt-5">
-      <Card />
+      <div className="grid grid-cols-4 items-center gap-3">
+        {products?.map((product) => (
+          <Card
+            key={product?.id}
+            imageUrl={product?.images[0]?.src}
+            description={product?.handle}
+            price={product?.variants[0]?.price?.amount}
+            productId={product?.id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
