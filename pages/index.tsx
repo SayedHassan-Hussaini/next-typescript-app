f7cc92d13ef3b09a5739ffbeb4746f9a3e1bccf4
Sayedhassan-Hussaini <sh.hussaini1378@gmail.com>
2023-01-03 14:10:05 +0430

    git customer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,28 @@ import Card from "../components/card/Card-component";
 import { shopifyClient, parseShopifyResponse } from "../lib/shopify";
 
 interface IIndex {
-  products: [id: string, descriptionHtml: string, images: [], handle: string];
+  products: [];
+  user: [];
 }
 type productType = {
   id: string;
-  images: [];
+  images: [{ src: "" }];
   handle: string;
-  variants: [];
+  variants: [{ price: { amount: number } }];
 };
-const Index = ({ products }: IIndex) => {
-  console.log("products...........", products);
+const Index = ({ products, user }: IIndex) => {
+  console.log("products...........", user);
 
   return (
     <div className="md:w-[80%] mx-auto mt-5">
       <div className="grid grid-cols-4 items-center gap-3">
-        {products?.map((product) => (
+        {products?.map(({ id, images, handle, variants }: productType) => (
           <Card
-            key={product?.id}
-            imageUrl={product?.images[0]?.src}
-            description={product?.handle}
-            price={product?.variants[0]?.price?.amount}
-            productId={product?.id}
+            key={id}
+            imageUrl={images[0]?.src}
+            description={handle}
+            price={variants[0]?.price?.amount}
+            productId={id}
           />
         ))}
       </div>
@@ -33,10 +34,20 @@ export default Index;
 export const getServerSideProps = async () => {
   // Fetch all the products
   const products = await shopifyClient.product.fetchAll();
-
+  const users = await fetch(
+    `https://sayed-company.myshopify.com/admin/api/2023-01/customers.json`,{
+      headers:{
+        'Content-Type': 'application/json',
+        'X-Shopify-Access-Token':"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"
+      }
+    }
+  );
+  const response= await users.json()
+  console.log("user..........", response);
   return {
     props: {
       products: parseShopifyResponse(products),
+      user: response,
     },
   };
 };
